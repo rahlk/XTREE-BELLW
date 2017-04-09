6fc5497ea44cd078af7b7f8f1d9a7c9d82133d1e
rahlk <i.m.ralk@gmail.com>
2017-04-08 20:54:38 -0400

    Autocommit

--- src/Data/DefectPrediction/DefectData.py
+++ src/Data/DefectPrediction/DefectData.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-root = os.path.join("/".join(os.getcwd().split('/')[:-1]))
+root = os.path.join(os.getcwd().split('src')[0], 'src')
+
 if root not in sys.path:
     sys.path.append(root)
 from glob import glob
